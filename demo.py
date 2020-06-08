b9d30413ad8f593cbcbbab52e55745c60b88162b
abelttitus <abelttitus1998@gmail.com>
2020-06-08 21:32:12 +0530

    helo

--- demo.py
+++ demo.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
   # frustums in the dataset
   # ======================================================================================================== #
   print("Estimating voxel volume bounds...")
-  n_imgs = 1508
+  n_imgs = 573
   cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
   print "Camera Intrinsics",cam_intr
-  cam_poses=np.loadtxt("data/gt_poses.txt")
+  cam_poses=np.loadtxt("data/pose_associateq.txt")
   #print "Cam poses shape",cam_poses.shape
   vol_bnds = np.zeros((3,2))
   for i in range(n_imgs):
@@ -27,12 +27,12 @@ if __name__ == "__main__":
     depth_im = cv2.imread("/home/ashfaquekp/depth/%d.png"%(i+1),-1).astype(float)
     depth_im /= 5000.  # depth is saved in 16-bit PNG in millimeters
     #print "Shape of depth image:",depth_im.shape
-    #depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset)
-    #cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))  # 4x4 rigid transformation matrix
+
     cam_pose=cam_poses[3*i:3*(i+1),:]
     homo=np.array([[0.0,0.0,0.0,1.0]])
     cam_pose=np.concatenate([cam_pose,homo])
     #print "Concatenated Cam pose",cam_pose
+
     # Compute camera view frustum and extend convex hull
     view_frust_pts = fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
     vol_bnds[:,0] = np.minimum(vol_bnds[:,0], np.amin(view_frust_pts, axis=1))
---
+++ my_demo.py
@@ -0,0 +1,69 @@
+import time
+
+
+import numpy as np
+
+_EPS = np.finfo(float).eps * 4.0
+def transform44(l):
+    """
+    Generate a 4x4 homogeneous transformation matrix from a 3D point and unit quaternion.
+    
+    Input:
+    l -- tuple consisting of (stamp,tx,ty,tz,qx,qy,qz,qw) where
+         (tx,ty,tz) is the 3D position and (qx,qy,qz,qw) is the unit quaternion.
+         
+    Output:
+    matrix -- 4x4 homogeneous transformation matrix
+    """
+    t = l[1:4]
+    q = np.array(l[4:8], dtype=np.float64, copy=True)
+    nq = np.dot(q, q)
+    if nq < _EPS:
+        return np.array((
+        (                1.0,                 0.0,                 0.0, t[0])
+        (                0.0,                 1.0,                 0.0, t[1])
+        (                0.0,                 0.0,                 1.0, t[2])
+        (                0.0,                 0.0,                 0.0, 1.0)
+        ), dtype=np.float64)
+    q *= np.sqrt(2.0 / nq)
+    q = np.outer(q, q)
+    return np.array((
+        (1.0-q[1, 1]-q[2, 2],     q[0, 1]-q[2, 3],     q[0, 2]+q[1, 3], t[0]),
+        (    q[0, 1]+q[2, 3], 1.0-q[0, 0]-q[2, 2],     q[1, 2]-q[0, 3], t[1]),
+        (    q[0, 2]-q[1, 3],     q[1, 2]+q[0, 3], 1.0-q[0, 0]-q[1, 1], t[2]),
+        (                0.0,                 0.0,                 0.0, 1.0)
+        ), dtype=np.float64)
+
+if __name__ == "__main__":
+  # ======================================================================================================== #
+  # (Optional) This is an example of how to compute the 3D bounds
+  # in world coordinates of the convex hull of all camera view
+  # frustums in the dataset
+  # ======================================================================================================== #
+  print("Estimating voxel volume bounds...")
+  n_imgs = 573
+  cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
+  print "Camera Intrinsics",cam_intr
+  cam_poses=np.loadtxt("data/pose_associateq.txt")
+  print "Cam poses shape",cam_poses.shape
+  vol_bnds = np.zeros((3,2))
+	
+  base_dir="/home/ashfaquekp/"
+  file = open("associate.txt")
+  data = file.read()
+  lines = data.split("\n") 
+  
+  for line in lines:     #This is used to loop all images
+    line=line.split(" ")
+    rgb_file=contents[1]
+    depth_file=contents[3]
+    depth_im = cv2.imread(base_dir+depth_file,-1).astype(float)
+    cv2.imshow(depth_im)
+    rgb_im = cv2.imread(base_dir+rgb_file,-1).astype(float)
+    cv2.imshow(rgb_im)
+    cam_pose=transform44(cam_poses[0,:])
+    print cam_pose
+    depth_im /= 5000.  # depth is saved in 16-bit PNG in millimeters
+    break
+
+  
