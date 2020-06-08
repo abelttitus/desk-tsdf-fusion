aaa09d1feb3380a16c328dd9b1574bb9cf5367ad
abelttitus <abelttitus1998@gmail.com>
2020-06-08 22:39:33 +0530

    15

--- my_demo.py
+++ my_demo.py
@@ -75,7 +75,54 @@ if __name__ == "__main__":
     vol_bnds[:,0] = np.minimum(vol_bnds[:,0], np.amin(view_frust_pts, axis=1))
     vol_bnds[:,1] = np.maximum(vol_bnds[:,1], np.amax(view_frust_pts, axis=1))
   print "Volume Bounds:",vol_bnds
-  print "Index:",index
+  file.close()
+
+  # Integrate
+  # ======================================================================================================== #
+  # Initialize voxel volume
+  print("Initializing voxel volume...")
+  tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.02)
+
+  # Loop through RGB-D images and fuse them together
+  t0_elapse = time.time()
+
+  file = open("associate.txt")
+  data = file.read()
+  lines = data.split("\n") 
+  
+  i=0
+  for line in lines:     #This is used to loop all images
+    contents=line.split(" ")
+    print("Fusing frame %d/%d"%(i+1, n_imgs))
+    try:
+      rgb_file=base_dir+contents[1]
+      depth_file=base_dir+contents[3]
+    except:
+      continue
+
+    # Read RGB-D image and camera pose
+    color_image = cv2.cvtColor(cv2.imread(rgb_file), cv2.COLOR_BGR2RGB)
+    depth_im = cv2.imread(depth_file,-1).astype(float)
+    depth_im /= 5000.
+    #depth_im[depth_im == 65.535] = 0
+    cam_pose=transform44(cam_poses[index,:])
+    
+
+    # Integrate observation into voxel volume (assume color aligned with depth)
+    tsdf_vol.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
+
+  fps = n_imgs / (time.time() - t0_elapse)
+  print("Average FPS: {:.2f}".format(fps))
+
+  # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
+  print("Saving mesh to mesh.ply...")
+  verts, faces, norms, colors = tsdf_vol.get_mesh()
+  fusion.meshwrite("mesh-desk1.ply", verts, faces, norms, colors)
+
+  # Get point cloud from voxel volume and save to disk (can be viewed with Meshlab)
+  print("Saving point cloud to pc.ply...")
+  point_cloud = tsdf_vol.get_point_cloud()
+  fusion.pcwrite("pc-desk1.ply", point_cloud)
 
     
 
